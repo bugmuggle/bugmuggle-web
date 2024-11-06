a7387b09ab03ab78e5b72ce2ac605ae16d2fdb5b
Sai K K <saikksub@gmail.com>
2024-11-06 07:59:16 +0530

    refactor: focus compose on click

--- components/chat/compose.vue
+++ components/chat/compose.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="space-y-0 flex flex-col px-3 h-full pb-4">
+  <div class="space-y-0 flex flex-col px-3 h-full pb-4 cursor-text" @click="onClickCompose">
     <div class="flex items-center gap-3 h-12 py-3 sticky top-0 bg-gray-900">
       <UButton
         icon="i-heroicons-bold"
+        class="compose-action"
         variant="ghost"
         color="gray"
         @click="editor.commands.toggleBold()"
@@ -10,12 +11,14 @@
       <UButton
         icon="i-heroicons-italic"
         variant="ghost"
+        class="compose-action"
         color="gray"
         @click="editor.commands.toggleItalic()"
       />
       <UButton
         icon="i-heroicons-strikethrough"
         variant="ghost"
+        class="compose-action"
         color="gray"
         @click="editor.commands.toggleStrike()"
       />
@@ -25,6 +28,7 @@
       <UButton
         icon="i-heroicons-list-bullet"
         variant="ghost"
+        class="compose-action"
         color="gray"
         @click="editor.commands.toggleBulletList()"
       />
@@ -32,6 +36,7 @@
       <UButton
         icon="i-heroicons-numbered-list"
         variant="ghost"
+        class="compose-action"
         color="gray"
         @click="editor.commands.toggleOrderedList()"
       />
@@ -41,10 +46,10 @@
       <UButton
         icon="i-heroicons-link"
         variant="ghost"
+        class="compose-action"
         color="gray"
         @click="editor.commands.toggleLink()"
       />
-
 
     </div>
     <div class="px-3 cursor-text" @click="editor.commands.focus()">
@@ -55,6 +60,7 @@
       <UButton
         icon="i-heroicons-paper-airplane"
         variant="ghost"
+        class="compose-action"
         color="gray"
         @click="sendMessage"
       />
@@ -88,6 +94,13 @@ onMounted(() => {
     extensions: [StarterKit]
   })
 })
+
+const onClickCompose = (e) => {
+  // Don't focus if clicking on a compose action button
+  // if (e.target.closest('.compose-action')) return
+  
+  editor.value.commands.focus()
+}
 
 onBeforeUnmount(() => {
   editor.value.destroy()
