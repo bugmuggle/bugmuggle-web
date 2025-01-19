5519db0184483f5d87529dd2979f1422018309ea
Sai K K <saikksub@gmail.com>
2025-01-19 20:23:08 +0530

    feat: check auth in splash page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import "./App.css";
+import pb from "./lib/pocketbase/client";
 
 function App() {
+  useEffect(() => {
+    if (!pb.authStore.isValid) {
+      console.log('not auth')
+    }
+  }, [])
+
   return (
     <div className="h-screen flex items-center justify-center">
       <span className="loading loading-spinner loading-lg"></span>
