835d32f72912104c6870b4a961a89fe181d6cc4d
Sai K K <saikksub@gmail.com>
2025-03-25 20:26:54 +0530

    fix: unable to mutate local updated task

--- server/api/channel/[cid]/tasks/[tid]/update.post.js
+++ server/api/channel/[cid]/tasks/[tid]/update.post.js
@@ -10,16 +10,17 @@ export default defineAuthEventHandler(async (event) => {
   const githubId = (await getUserSession(event)).user.githubId
   const user = await getUserByGithubId(githubId)
 
-  const task = await useDrizzle().update(tables.tasks)
+  const snapshotUpdateTask = await useDrizzle().update(tables.tasks)
     .set({
       ...updates,
       updatedAt: new Date(),
       updatedBy: user.id,
     })
     .where(eq(tables.tasks.id, tid))
+    .returning()
 
   return {
     success: true,
-    data: task[0]
+    data: snapshotUpdateTask[0]
   }
 })
