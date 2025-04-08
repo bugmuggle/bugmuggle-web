97137ccfe628ec2907b5845eabba66fc2d88a3cd
kandalgaonkarshubham <kandalgaonkarshubham@gmail.com>
2025-04-08 11:11:57 +0530

    feat: send user metadata when creating task

--- server/api/channel/[cid]/tasks/create.post.js
+++ server/api/channel/[cid]/tasks/create.post.js
@@ -14,8 +14,19 @@ export default defineAuthEventHandler(async (event) => {
     updatedAt: new Date(),
   }).returning()
 
+  // Fetch the complete task data with creator information
+  const completeTask = await useDrizzle()
+    .select({
+      ...tables.tasks,
+      createdByGithubAvatarUrl: tables.users.githubAvatarUrl,
+      createdByGithubUsername: tables.users.githubUsername,
+    })
+    .from(tables.tasks)
+    .where(eq(tables.tasks.id, task[0].id))
+    .leftJoin(tables.users, eq(tables.tasks.createdBy, tables.users.id))
+
   return {
     success: true,
-    data: { task: task[0] },
+    data: { task: completeTask[0] },
   }
 })
